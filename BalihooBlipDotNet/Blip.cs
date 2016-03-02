701b731559f279e5dbbe171dc3ae152b52e294ff
balihoo-dengstrom <dengstrom@balihoo.com>
2016-03-02 16:28:06 -0700

    WIP: Add bulkLoad support

--- BalihooBlipDotNet/Blip.cs
+++ BalihooBlipDotNet/Blip.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BalihooBlipDotNet
 {
@@ -28,22 +28,22 @@ namespace BalihooBlipDotNet
         /// Ping the BLIP API.
         /// </summary>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> Ping()
+        public BlipResponse Ping()
         {
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Get, "/ping");
+            return request.ExecuteCommand(BlipRequest.Command.Get, "/ping").Result;
         }
 
         /// <summary>
         /// Get a list of brandKeys that the API user is authorized to access.
         /// </summary>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> GetBrandKeys()
+        public BlipResponse GetBrandKeys()
         {
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Get, "/brand");
+            return request.ExecuteCommand(BlipRequest.Command.Get, "/brand").Result;
         }
 
         /// <summary>
@@ -51,12 +51,12 @@ namespace BalihooBlipDotNet
         /// </summary>
         /// <param name="brandKey">The unique identifier for a single brand.</param>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> GetBrandSources(string brandKey)
+        public BlipResponse GetBrandSources(string brandKey)
         {
             var path = $"/brand/{brandKey}/source";
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Get, path);
+            return request.ExecuteCommand(BlipRequest.Command.Get, path).Result;
         }
 
         /// <summary>
@@ -64,12 +64,12 @@ namespace BalihooBlipDotNet
         /// </summary>
         /// <param name="brandKey">The unique identifier for a single brand.</param>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> GetBrandProjections(string brandKey)
+        public BlipResponse GetBrandProjections(string brandKey)
         {
             var path = $"/brand/{brandKey}/projection";
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Get, path);
+            return request.ExecuteCommand(BlipRequest.Command.Get, path).Result;
         }
 
         /// <summary>
@@ -78,12 +78,12 @@ namespace BalihooBlipDotNet
         /// <param name="brandKey">The unique identifier for a single brand.</param>
         /// <param name="projection">Optionally filter data to locations in a single projecttion. Defaults to "universal".</param>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> GetLocationKeys(string brandKey, string projection="universal")
+        public BlipResponse GetLocationKeys(string brandKey, string projection="universal")
         {
             var path = $"/brand/{brandKey}/location?projection={projection}";
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Get, path);
+            return request.ExecuteCommand(BlipRequest.Command.Get, path).Result;
         }
 
         /// <summary>
@@ -94,12 +94,12 @@ namespace BalihooBlipDotNet
         /// <param name="projection">Optionally filter data in a single projection. Defaults to "universal".</param>
         /// <param name="includeRefs">Optionally include objects referenced by the location in its data. Defaults to false.</param>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> GetLocation(string brandKey, string locationKey, string projection="universal", bool includeRefs=false)
+        public BlipResponse GetLocation(string brandKey, string locationKey, string projection="universal", bool includeRefs=false)
         {
             var path = $"/brand/{brandKey}/location/{locationKey}?projection={projection}&includeRefs={includeRefs.ToString().ToLower()}";
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Get, path);
+            return request.ExecuteCommand(BlipRequest.Command.Get, path).Result;
         }
 
         /// <summary>
@@ -109,13 +109,13 @@ namespace BalihooBlipDotNet
         /// <param name="query">A stringified JSON query used to filter locations in BLIP.</param>
         /// <param name="view">Optionally specify the view returned. Defaults to "full".</param>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> QueryLocations(string brandKey, string query, string view="full")
+        public BlipResponse QueryLocations(string brandKey, string query, string view="full")
         {
             var path = $"/brand/{brandKey}/locationList";
             var queryParam = $"{{\"query\": {query}, \"view\": \"{view}\"}}";
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Post, path, queryParam);
+            return request.ExecuteCommand(BlipRequest.Command.Post, path, queryParam).Result;
         }
 
         /// <summary>
@@ -126,12 +126,12 @@ namespace BalihooBlipDotNet
         /// <param name="source">The unique identifier for the data source being used to add/update the location.</param>
         /// <param name="locationData">The stringified JSON location document.</param>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> PutLocation(string brandKey, string locationKey, string source, string locationData)
+        public BlipResponse PutLocation(string brandKey, string locationKey, string source, string locationData)
         {
             var path = $"/brand/{brandKey}/location/{locationKey}?source={source}";
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Put, path, locationData);
+            return request.ExecuteCommand(BlipRequest.Command.Put, path, locationData).Result;
         }
 
         /// <summary>
@@ -141,12 +141,76 @@ namespace BalihooBlipDotNet
         /// <param name="locationKey">The unique identifier for a single location within the brand.</param>
         /// <param name="source">The unique identifier for the data source being used to delete the location.</param>
         /// <returns>BlipResponse object with a status code and body text if applicable.</returns>
-        public Task<BlipResponse> DeleteLocation(string brandKey, string locationKey, string source)
+        public BlipResponse DeleteLocation(string brandKey, string locationKey, string source)
         {
             var path = $"/brand/{brandKey}/location/{locationKey}?source={source}";
             var request = new BlipRequest(Credentials, Endpoint);
 
-            return request.ExecuteCommand(BlipRequest.Command.Delete, path);
+            return request.ExecuteCommand(BlipRequest.Command.Delete, path).Result;
+        }
+
+        public BlipResponse BulkLoad(string brandKey, string source, string filePath, bool implicitDelete,
+            int expectedRecordCount, string successEmail = null, string failEmail = null,
+            string successCallbackUrl = null, string failCallbackUrl = null)
+        {
+            // Use pre-seigned auth from BLIP to upload the file to S3
+            var blipRequest = new BlipRequest(Credentials, Endpoint);
+            var s3UploadResponse = new S3Request().Upload(blipRequest, brandKey, filePath);
+
+            if (s3UploadResponse.StatusCode != 204) return s3UploadResponse;
+
+            var path = $"/brand/{brandKey}/bulkLoad";
+            var s3Path = s3UploadResponse.Body;
+            path += $"s3Path={s3Path}&source={source}&implicitDelete={implicitDelete}&expectedRecordCount={expectedRecordCount}";
+
+            // Validate and add optional params
+            if (!string.IsNullOrEmpty(successEmail))
+            {
+                if (IsValidEmail(successEmail))
+                    path += $"&successEmail={successEmail}";
+                else
+                    return new BlipResponse(400, $"Error: successEmail is not valid. {successEmail}");
+
+                if (IsValidEmail(failEmail))
+                    path += $"&failEmail={failEmail}";
+                else
+                    return new BlipResponse(400, $"Error: failEmail is not valid. {failEmail}");
+
+                if (IsValidUrl(successCallbackUrl))
+                    path += $"&successCallbackUrl={successCallbackUrl}";
+                else
+                    return new BlipResponse(400, $"Error: successCallbackUrl is not valid. {successCallbackUrl}");
+
+                if (IsValidUrl(failCallbackUrl))
+                    path += $"&failCallbackUrl={failCallbackUrl}";
+                else
+                    return new BlipResponse(400, $"Error: failCallbackUrl is not valid. {failCallbackUrl}");
+            }
+            return blipRequest.ExecuteCommand(BlipRequest.Command.Post, path).Result;
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email)) return false;
+            try
+            {
+                // Return false if any email address in the list is invalid. Otherwise return true.
+                return !(from emailString in email.Split(',')
+                         let addr = new System.Net.Mail.MailAddress(emailString)
+                         where addr.Address != emailString
+                         select emailString).Any();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool IsValidUrl(string url)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(url, UriKind.Absolute, out uriResult) 
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
         }
     }
 }
---
+++ BalihooBlipDotNet/S3Request.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BalihooBlipDotNet
+{
+    internal class S3Request
+    {
+        internal BlipResponse Upload(BlipRequest blipRequest, string brandKey, string filePath)
+        {
+            // Compress file contents
+            var byteStream = File.ReadAllBytes(filePath);
+            using (var fs = new FileStream(filePath, FileMode.CreateNew))
+            using (var gzip = new GZipStream(fs, CompressionMode.Compress, false))
+            {
+                gzip.Write(byteStream, 0, byteStream.Length);
+            }
+
+            // Get authorization to upload file from BLIP
+            var fileMd5 = GenerateMd5Hash(byteStream);
+            var path = $"/brand/{brandKey}/authorizeUpload?fileMD5={fileMd5}";
+            var authResponse = blipRequest.ExecuteCommand(BlipRequest.Command.Get, path).Result;
+
+            if (authResponse.StatusCode != 200) return authResponse; // Return error response if auth fails
+
+            var mimeType = "text/plain"; // TODO: Inspect file for MIME type
+            dynamic auth = JsonConvert.DeserializeObject(authResponse.Body);
+            string formData = auth.data;
+            string s3Bucket = auth.s3Bucket;
+            string s3Key = auth.data.key;
+            var s3Path = $"s3://{s3Bucket}/{s3Key}";
+
+            var uploadResponse = PostFile(s3Bucket, formData, mimeType, byteStream);
+
+            // If upload fails Return error response else return success response
+            return uploadResponse.StatusCode != 204 ? uploadResponse : new BlipResponse(uploadResponse.StatusCode, s3Path);
+        }
+
+        private static BlipResponse PostFile(string s3Bucket, string formData, string mimeType, byte[] compressedFile)
+        {
+            HttpResponseMessage response;
+            dynamic data = JsonConvert.DeserializeObject(formData);
+
+            using (var client = new HttpClient())
+            {
+                var requestContent = new MultipartFormDataContent();
+                var fileContent = new ByteArrayContent(compressedFile);
+                fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse(mimeType);
+
+                requestContent.Add(new StringContent(data.acl), "acl");
+                requestContent.Add(new StringContent(data.bucket), "bucket");
+                requestContent.Add(new StringContent(data.key), "key");
+                requestContent.Add(new StringContent(JObject.Parse(data)["conent-md5"]), "content-md5");
+                requestContent.Add(new StringContent(data.policy), "policy");
+                requestContent.Add(new StringContent(data.signature), "signature");
+                requestContent.Add(new StringContent(data.AWSAccessKeyId), "AWSAccessKeyId");
+                requestContent.Add(new StringContent(mimeType), "content-type");
+                requestContent.Add(fileContent, "file"); // The file must be added last
+
+                var url = $"https://s3.amazonaws.com/{s3Bucket}";
+                response = client.PostAsync(url, requestContent).Result;
+            }
+
+            var statusCode = (int)response.StatusCode;
+
+            // If the upload is not successful return the error message from S3 else return success response
+            return statusCode != 204 ? new BlipResponse(statusCode, response.Content.ToString()) : new BlipResponse(200, "");
+        }
+
+        private static string GenerateMd5Hash(byte[] content)
+        {
+            var sb = new StringBuilder();
+            var fileMd5 = MD5.Create();
+            var hash = fileMd5.ComputeHash(content);
+
+            foreach (var hashByte in hash)
+            {
+                sb.Append(hashByte.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
